e49e0bf4f7573c4aa6d696accab9049dbf2e71ca
Axel <dsg4857@gmail.com>
2026-04-08 17:14:09 -0500

    refactor: Cambiar tipo de ID en CamionRepository y añadir método para filtrar camiones disponibles

--- src/main/java/com/cibertec/demo/repository/CamionRepository.java
+++ src/main/java/com/cibertec/demo/repository/CamionRepository.java
@@ -4,6 +4,9 @@ import com.cibertec.demo.modelo.Camion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CamionRepository extends JpaRepository<Camion, Integer> {
+public interface CamionRepository extends JpaRepository<Camion, Long> {
+    List<Camion> findByDisponibilidadTrue();
 }
