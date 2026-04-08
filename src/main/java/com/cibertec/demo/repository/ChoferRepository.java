d64a8a33c409f7359623b29116c25cbbbf6fe922
Axel <dsg4857@gmail.com>
2026-04-08 17:46:32 -0500

    delete: eliminar clases innecesarias

--- src/main/java/com/cibertec/demo/repository/ChoferRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cibertec.demo.repository;
-
-import com.cibertec.demo.modelo.Chofer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChoferRepository extends JpaRepository<Chofer, Integer> {
-}
